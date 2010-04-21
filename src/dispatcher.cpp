efd2d301c9eb97d09f04881d4cf94b5de1d44de3
rednax@ubuntu.ubuntu-domain <rednax@ubuntu.ubuntu-domain>
2010-04-21 14:58:49 +0200

    oops

---
+++ src/dispatcher.cpp
@@ -0,0 +1,20 @@
+#include "cow/dispatcher.hpp"
+
+#include <iostream>
+
+dispatcher::dispatcher()
+    : work(io_service),
+      thread(boost::bind(&boost::asio::io_service::run, &io_service))
+{
+
+}
+
+dispatcher::~dispatcher()
+{
+#ifdef DEBUG
+    std::cerr << "dispatcher: waiting for running jobs to complete..."
+              << std::endl;
+#endif
+    io_service.stop();
+    thread.join();
+}
