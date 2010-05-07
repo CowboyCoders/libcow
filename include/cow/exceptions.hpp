801b0ec3d5b50e00f7d710c781dcf3657f4b2297
moshisushi <devnull@localhost>
2010-05-07 09:49:30 +0200

    exceptions include file

---
+++ include/cow/exceptions.hpp
@@ -0,0 +1,40 @@
+#ifndef ___libcow_exceptions___
+#define ___libcow_exceptions___
+
+#include "cow/libcow_def.hpp"
+#include <exception>
+#include <string>
+
+namespace libcow 
+{
+    class LIBCOW_EXPORT exception: public std::exception
+    {
+    public:
+        exception(const std::string& message) 
+            : msg(message)
+        {
+        }
+        
+        virtual const char* what() const throw()
+        {
+            return msg.c_str();
+        }
+
+    private:
+        std::string msg;
+    };
+
+    class LIBCOW_EXPORT error_message
+    {
+    public:
+        error_message() : msg("Unknown error") {}
+        error_message(const std::string& message) : msg(message) {}
+        void set(const std::string& message) { msg = message; }
+        std::string const& get() const { return msg; }
+    private:
+        std::string msg;
+    };
+}
+
+
+#endif // ___libcow_exceptions___
